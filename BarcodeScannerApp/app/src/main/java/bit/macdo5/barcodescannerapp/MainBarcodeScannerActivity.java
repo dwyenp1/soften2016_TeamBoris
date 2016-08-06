21da5fa9f1af10697dadc8cf47bc6d33e625b1ee
Oscar Macdonald <macdo5@student.op.ac.nz>
2016-08-06 21:44:43 +1200

    created basic barcode scanner app, added to my branch

---
+++ BarcodeScannerApp/app/src/main/java/bit/macdo5/barcodescannerapp/MainBarcodeScannerActivity.java
@@ -0,0 +1,49 @@
+package bit.macdo5.barcodescannerapp;
+/**
+ * Created by Oscar Macdonald on 6/08/2016.
+ * Made using https://android-coffee.com/tutorial-how-to-create-barcode-reader-app-in-android-studio-1-4/
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainBarcodeScannerActivity extends AppCompatActivity implements OnClickListener {
+    private Button scanBtn;
+    private TextView formatTxt;
+    private TextView contentTxt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_barcode_scanner);
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        if(v.getId()==R.id.scan_button){
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}

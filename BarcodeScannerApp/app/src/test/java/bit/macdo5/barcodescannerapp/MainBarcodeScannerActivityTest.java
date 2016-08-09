e3dee0fc8ca6b382e5eecf88713b9bbf9131516e
Oscar Macdonald (1000021598) <MACDO5@student.op.ac.nz>
2016-08-09 12:32:54 +1200

    added JUnit4 test, unable to replicate IntentIntegrator or IntentResult

---
+++ BarcodeScannerApp/app/src/test/java/bit/macdo5/barcodescannerapp/MainBarcodeScannerActivityTest.java
@@ -0,0 +1,26 @@
+package bit.macdo5.barcodescannerapp;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by macdo5 on 9/08/2016.
+ */
+public class MainBarcodeScannerActivityTest {
+    private IntentResult scanResult;
+    @Before
+    public void setUp() throws Exception{
+
+        //scanResult = new IntentResult("stuff", "EAN13", 35, 0, "n");
+
+    }
+    @Test
+    public void testOnActivityResult() throws Exception {
+        scanIntegrator.set
+    }
+}
---
+++ BarcodeScannerApp/app/src/test/java/bit/macdo5/barcodescannerapp/myUnitTest.java
@@ -0,0 +1,19 @@
+package bit.macdo5.barcodescannerapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+/**
+ * Created by macdo5 on 9/08/2016.
+ */
+public class myUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void addition2_isCorrect() throws Exception {
+        assertEquals(5, 2 + 2);
+    }
+}
